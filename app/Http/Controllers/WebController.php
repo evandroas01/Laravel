658c5895b51a770c7dc7ea70df748a8634cc5285
Evandro Silva <evandroas01@gmail.com>
2022-04-14 23:21:24 -0300

    Criação da parte de SEO

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -2,32 +2,69 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Support\Seo;
 use Illuminate\Http\Request;
 
 class WebController extends Controller
 {
     public function home()
     {
-        return view('front.home');
+       $head = $this->seo->render(env('APP_NAME') . ' - UpInside Treinamentos',
+            'Curso de Laravel',
+            url('/'),
+            asset('image/img_bg_1.jpg'));
+
+        return view('front.home', [
+            'head' => $head,
+        ]);
     }
 
     public function course()
     {
-        return view('front.course');
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre o curso',
+            'Curso de Laravel',
+            route('course'),
+            asset('image/img_bg_1.jpg'));
+
+        return view('front.course', [
+            'head' => $head,
+        ]);
     }
 
     public function blog()
     {
-        return view('front.blog');
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre o curso',
+            'Pagina de blog, com artigos dos alunos',
+            route('blog'),
+            asset('image/img_bg_1.jpg'));
+
+        return view('front.blog', [
+            'head' => $head,
+        ]);
     }
 
     public function article()
     {
-        return view('front.article');
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre o curso',
+            'Leia nossos artigos atualizados',
+            route('article'),
+            asset('image/img_bg_1.jpg'));
+
+        return view('front.article', [
+            'head' => $head,
+        ]);
     }
 
     public function contact()
     {
-        return view('front.contact');
+        $head = $this->seo->render(env('APP_NAME') . ' - Sobre o curso',
+            'Entre em contato com a melhor escola de programação',
+            route('contact'),
+            asset('image/img_bg_1.jpg'));
+
+        return view('front.contact', [
+            'head' => $head,
+        ]);
     }
 }
